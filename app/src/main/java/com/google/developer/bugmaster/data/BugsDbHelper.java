b7a2a0e851c36079944ca7b0f531ca87cd319823
vinayrraj <vinayrraj@gmail.com>
2018-06-27 10:01:24 +0000

    Added Insect recordes into DB

--- app/src/main/java/com/google/developer/bugmaster/data/BugsDbHelper.java
+++ app/src/main/java/com/google/developer/bugmaster/data/BugsDbHelper.java
@@ -1,11 +1,17 @@
 package com.google.developer.bugmaster.data;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.google.developer.bugmaster.R;
+import com.google.developer.bugmaster.data.InsectContract.WeatherEntry;
+import com.google.developer.bugmaster.util.AppExecutors;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONException;
 
@@ -26,16 +32,46 @@ public class BugsDbHelper extends SQLiteOpenHelper {
 
     //Used to read data from res/ and assets/
     private Resources mResources;
+    private Context mContext;
 
     public BugsDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-
+        mContext = context;
         mResources = context.getResources();
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        //TODO: Create and fill the database
+    public void onCreate(final SQLiteDatabase sqLiteDatabase) {
+
+        final String SQL_CREATE_WEATHER_TABLE =
+
+                "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
+
+                        WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        WeatherEntry.COLUMN_FRIENDLY_NAME + " TEXT NOT NULL, " +
+                        WeatherEntry.COLUMN_SCIENTIFIC_NAME + " TEXT NOT NULL," +
+                        WeatherEntry.COLUMN_CLASSIFICATION + " TEXT NOT NULL, " +
+                        WeatherEntry.COLUMN_IMAGE_ASSET + " TEXT NOT NULL, " +
+                        WeatherEntry.COLUMN_DANGER_LEVEL + " INTEGER NOT NULL, " +
+                        " UNIQUE (" + WeatherEntry.COLUMN_SCIENTIFIC_NAME + ") ON CONFLICT REPLACE);";
+
+
+        sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
+
+        AppExecutors appExecutors = new AppExecutors();
+
+        appExecutors.diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    readInsectsFromResources(sqLiteDatabase);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
@@ -63,6 +99,15 @@ public class BugsDbHelper extends SQLiteOpenHelper {
 
         //Parse resource into key/values
         final String rawJson = builder.toString();
-        //TODO: Parse JSON data and insert into the provided database instance
+        ContentValues[] weatherValues = new Gson().fromJson(rawJson, new TypeToken<ContentValues[]>() {
+        }.getType());
+
+        if (weatherValues != null && weatherValues.length != 0) {
+            ContentResolver sunshineContentResolver = mContext.getContentResolver();
+
+            sunshineContentResolver.bulkInsert(
+                    WeatherEntry.CONTENT_URI,
+                    weatherValues);
+        }
     }
 }

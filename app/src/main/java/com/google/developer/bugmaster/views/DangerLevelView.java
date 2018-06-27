8b5125a1006d28bb7b9836eceab687b723494f33
Vinayraj Singh Solanki <vinayrraj@gmail.com>
2018-06-27 19:48:01 +0530

    Code refractoring

--- app/src/main/java/com/google/developer/bugmaster/views/DangerLevelView.java
+++ app/src/main/java/com/google/developer/bugmaster/views/DangerLevelView.java
@@ -12,11 +12,6 @@ import com.google.developer.bugmaster.R;
 //TODO: This class should be used in the insect list to display danger level
 public class DangerLevelView extends TextView {
 
-    private float strokeWidth;
-    Paint circlePaint;
-    int strokeColor, solidColor;
-    String Text;
-    Float size = -1f;
     private int mDangerLevel;
 
     public DangerLevelView(Context context) {
@@ -46,51 +41,12 @@ public class DangerLevelView extends TextView {
         }
         int color = Color.parseColor(getResources().getStringArray(R.array.dangerColors)[dangerLevel-1]);
         setBackgroundTintList(ColorStateList.valueOf(color));
-        //this.strokeColor = color;
-        //this.solidColor = color;
         setText(String.valueOf(dangerLevel));
-        //this.Text = ;
-        //this.size = value;
     }
 
     public int getDangerLevel() {
         //TODO:
         return mDangerLevel;
     }
-
-//    @Override
-//    public void draw(Canvas canvas) {
-//        final int diameter, radius, h, w;
-//
-//        circlePaint.setColor(solidColor);
-//        circlePaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-//        Paint strokePaint = new Paint();
-//        strokePaint.setColor(strokeColor);
-//        strokePaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-//
-//        //get Height and Width
-//        h = this.getHeight();
-//        w = this.getWidth();
-//
-//        diameter = ((h > w) ? h : w);
-//        radius = diameter / 2;
-//
-//        //setting Height and width
-//        this.setHeight(diameter);
-//        this.setWidth(diameter);
-//        this.setText(Text);
-//
-//        if (size != -1f) {
-//
-//            this.setTextSize(size);
-//        } else {
-//
-//            this.setTextSize(diameter / 5);
-//        }
-//
-//        canvas.drawCircle(diameter / 2, diameter / 2, radius - strokeWidth, circlePaint);
-//        super.draw(canvas);
-//    }
-
 
 }
